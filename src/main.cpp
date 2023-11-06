c7f205f9a3e5ed82b907e18340e5d5ebfc67a658
bennettb593 <122071365+bennettb593@users.noreply.github.com>
2023-11-06 03:04:44 +0000

    j

--- src/main.cpp
+++ src/main.cpp
@@ -6,42 +6,44 @@
 #include <assert.h>
 
 #define SOMEVALUE 4
-#define RIGHT 1
-#define LEFT 0
+#define FORWARD_SPEED 255
+#define TURN_SPEED 133
+#define SLOW_SPEED 133
 
 //Prototypes
-void to_pool();
-String mission();
-void navigation();
-void turn(struct controller_pins *, float angle, int motor_speed);
-void forward(struct controller_pins *, char axis, int coord, int motor_speed);
-void pool_navigate_aruco(struct controller_pins *pins);
-void motor_off(struct controller_pins *);
-void approach_pool(struct controller_pins *);
-float get_distance(struct controller_pins *);
-void pool_navigate_distance_sensor(struct controller_pins *);
-void obstacle_navigate_distance_sensor(struct controller_pins *);
-void begin_mission(struct controller_pins *);
+void pool_nav();
+void turn(float angle, int motor_speed);
+void forward(char axis, int coord, int motor_speed);
+void motor_off();
+float get_distance();
+void obstacle_navigate_distance_sensor();
+void begin_mission();
+void mission();
 
 //Arduino pin assignments
-struct controller_pins {
-    int in1;
-    int in2;
-    int in3;
-    int in4;
-    int ENA;
-    int ENB;
-    int trigPin;
-    int echoPin;
-    int tx;
-    int rx;
+enum controller_pins {
+    in1 = 12,
+    in2 = 11,
+    in3 = 10,
+    in4 = 9,
+    ENA = 13,
+    ENB = 18,
+    trigPin = 24,
+    echoPin = 26,
+    tx = 53,
+    rx = 52,
+    salt_in = A0,
+    salt_out
+
+
 };
+
 
 //initialize all input and output pins
 
 void setup() {
-    int output[] = {12,11,10,9,24,50,52};
-    int input[] = {26};
+    int output[] = {in1, in2, in3, in4, trigPin};
+    int input[] = {echoPin};
     int num_output = sizeof(output) / 2;
     int num_input = sizeof(input) / 2;
 
@@ -56,11 +58,15 @@ void setup() {
 
 
 void loop() {
-    struct controller_pins pins =  {12, 11, 10, 9, 13, 18, 24, 26, 50, 52};
-    begin_mission(&pins);
-    //drive to pool
-    pool_navigate_aruco(&pins);
-    approach_pool(&pins);
+    //Initial setup
+    motor_off();
+    begin_mission();
+
+    //navigate to pool
+    pool_nav();
+    //mission
+    mission();
+
 
 
 
@@ -73,11 +79,14 @@ while(1);//loop will not continue
 
 }
 
-void forward(struct controller_pins *pins, char axis, int coord, int motor_speed){
-    analogWrite(pins->ENA, motor_speed);
-    analogWrite(pins->ENB, motor_speed);
-    digitalWrite(pins->in1, HIGH);
-    digitalWrite(pins->in3, HIGH);
+void forward(char axis, int coord, int motor_speed){
+    //Turn on motors forward
+    analogWrite(ENA, motor_speed);
+    analogWrite(ENB, motor_speed);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    digitalWrite(in2, LOW);
     int position;
     if (axis == 'x')
         position = Enes100.getX();
@@ -90,123 +99,82 @@ void forward(struct controller_pins *pins, char axis, int coord, int motor_speed
             position = Enes100.getY();
 
     }
-    motor_off(pins);
+    motor_off();
 
     
 
 }
 
-void turn(struct controller_pins *pins, float angle, int motor_speed){
+void turn(float angle, int motor_speed){
     float heading = Enes100.getTheta();
-    analogWrite(pins->ENA, motor_speed);
-    analogWrite(pins->ENB, motor_speed);
+    analogWrite(ENA, motor_speed);
+    analogWrite(ENB, motor_speed);
+
+    digitalWrite(in1, LOW);
+    digitalWrite(in4, LOW);
+    digitalWrite(in1, LOW);
+    digitalWrite(in4, LOW);
     //while heading of otv is greater than some minimal limit of target angle, turn one direction
     while (abs(angle - heading) > SOMEVALUE){
         if (angle - heading < 0){
-            digitalWrite(pins->in1, HIGH);
-            digitalWrite(pins->in4, HIGH);
+            digitalWrite(in1, HIGH);
+            digitalWrite(in4, HIGH);
+          
             
 
         } else {
-            digitalWrite(pins->in2, HIGH);
-            digitalWrite(pins->in3, HIGH);
+            digitalWrite(in2, HIGH);
+            digitalWrite(in3, HIGH);
         }
 
     }
-    motor_off(pins);
+    motor_off();
 
     
 
 
 }
 
-void motor_off(struct controller_pins *pins){
-    digitalWrite(pins->in1, LOW);
-    digitalWrite(pins->in2, LOW);
-    digitalWrite(pins->in3, LOW);
-    digitalWrite(pins->in4, LOW);
-}
-
-
-void enes_init(){
-    //Enes100.begin(const char Wet_Willies, byte WATER, int markerId, int wifiModuleTX, int wifiModuleRX);
-    while(!Enes100.isConnected());
-
-
-}
-
-void pool_navigate_aruco(struct controller_pins *pins){
-    //get desired angle and position based on starting location
-    float angle;
-    float coord;
-    //
-
-    //turn to pool angle
-    turn(pins, angle, 255);
-    //move foward some amount
-    forward(pins, 'x', coord, 255);
-
-    //get sensor reading
-    
+void motor_off(){
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
 }
 
 
 
-  void approach_pool(struct controller_pins *pins){
-    int motor_speed;
-    int dist = get_distance(pins);
-    while (dist > SOMEVALUE){
-        analogWrite(pins->ENA, motor_speed);
-        analogWrite(pins->ENB, motor_speed);
-        digitalWrite(pins->in1, HIGH);
-        digitalWrite(pins->in3, HIGH);
-        dist = get_distance(pins);
-    }
-    motor_off(pins);
-    
 
-  }
+
+
 
 //returns sensor distance in cm
-float get_distance(struct controller_pins *pins) {
+float get_distance() {
   float duration, distance;
-  digitalWrite(pins->trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pins->trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pins->trigPin, LOW);
-
-  noInterrupts(); 
-  duration = pulseIn(pins->echoPin, HIGH); 
-  interrupts();
-
+  delay(100);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(200);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
   distance = (duration*.0343)/2;
   return distance;
-  delay(100);
-}
 
-void pool_navigate_distance_sensor(struct controller_pins *pins){
-  float stop_distance = 17.496;
-
-  //forward
-  while(get_distance(pins) > stop_distance){
-
-  }
-  motor_off(pins);
 }
 
 
-void obstacle_navigate_distance_sensor(struct controller_pins *pins){
+void obstacle_navigate_distance_sensor(){
   float stop_distance = 12;
 
   //forward
-  while(get_distance(pins) > stop_distance){
+  while(get_distance() > stop_distance){
 
   }
-  motor_off(pins);
-  turn(pins, 1.57, 255);
-  if(get_distance(pins) < stop_distance){
-    turn(pins, -1.57, 255);
+  motor_off();
+  turn(1.57, 255);
+  if(get_distance() < stop_distance){
+    turn(-1.57, 255);
   }
   else
   {
@@ -214,6 +182,75 @@ void obstacle_navigate_distance_sensor(struct controller_pins *pins){
   }
 }
 
-  void begin_mission(struct controller_pins *pins){
-      //Enes100.begin(const char* teamName, byte teamType, int markerId, int wifiModuleTX, int wifiModuleRX);
+  void begin_mission(){
+    Enes100.begin("Wet Willies", WATER, 19, tx, rx);
+    Enes100.println("Connected to vision system.");
   }
+
+
+
+
+
+void pool_nav(){
+
+
+    /* 
+    
+    
+    drive to pool
+
+    */
+   
+    //get desired angle and position based on starting location
+    float angle;
+    float coord;
+
+    if (Enes100.getY() < 1){
+        angle = M_PI/2;
+        coord = 1.2;
+    }else{
+        angle = -M_PI/2;
+        coord = 0.8;
+    }
+
+    Enes100.print("Drive to angle: ");
+    Enes100.println(angle);
+    Enes100.println("Drive to y: ");
+    Enes100.println(coord);
+    //turn to pool angle
+    turn(angle, TURN_SPEED);
+    //move foward some amount
+    forward('y', coord, FORWARD_SPEED);
+  
+
+    int dist;
+    analogWrite(ENA, SLOW_SPEED);
+    analogWrite(ENB, SLOW_SPEED);
+    while ((dist = get_distance()) > SOMEVALUE){
+        
+        digitalWrite(in2, LOW);
+        digitalWrite(in4, LOW);
+        digitalWrite(in1, HIGH);
+        digitalWrite(in3, HIGH);
+    }
+    motor_off();
+}
+
+void mission(){
+    //get depth
+    String salt;
+
+
+    //get salinity
+    digitalWrite(salt_out, HIGH);
+    delay(500);
+    while (analogRead(salt_in) < SOMEVALUE);
+    delay(1000);
+    //Enes100.println(analogRead(salt_in));
+    salt = (analogRead(salt_in) > 400) ? "salted" : "not salted";
+    Enes100.print("This water is: ");
+    Enes100.println(salt);
+
+
+
+}
